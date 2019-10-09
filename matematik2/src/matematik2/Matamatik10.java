591e3ded56d59b32bdd0edaf41de8a2f7b131059
BOSS <BOSS@DESKTOP-4M1S8HA>
2019-10-09 17:07:31 +0300

    mat

---
+++ matematik2/src/matematik2/Matamatik10.java
@@ -0,0 +1,15 @@
+package matematik2;
+
+public class Matamatik10 {
+
+	public static void main(String[] args) {
+		// matematik9
+		int a=4;
+		int b=6;
+		int c=8;
+		
+		System.out.println ((a^2+b^2)^(1/3)+a*b*c/(a+b/((c-b/a^3)^(1/2))));
+
+	}
+
+}
