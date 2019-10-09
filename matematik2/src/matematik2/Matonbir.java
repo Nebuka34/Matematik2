5e5810c5dfed9cb5308e2cf19fd4a9ba3100f4ab
BOSS <BOSS@DESKTOP-4M1S8HA>
2019-10-09 17:24:56 +0300

    mat11

---
+++ matematik2/src/matematik2/Matonbir.java
@@ -0,0 +1,17 @@
+package matematik2;
+
+public class Matonbir {
+
+	public static void main(String[] args) {
+		// Mat11
+		int a=4;
+		int b=6;
+		int c=8;
+		
+		System.out.println (((a-b)^(1/5)/(a+b-c/(a*b))^(1/4))^(1/3)+1/((1+1/(1+1/(a*b*c))^(1/2)))^(1/2));
+		
+		
+
+	}
+
+}
