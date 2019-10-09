b5f445fdc1c884747450dde0e33fc0dfd31ee68d
BOSS <BOSS@DESKTOP-4M1S8HA>
2019-10-09 17:17:24 +0300

    mat11

---
+++ matematik2/src/matematik2/Mat11.java
@@ -0,0 +1,14 @@
+package matematik2;
+
+public class Mat11 {
+
+	public static void main(String[] args) {
+		// mat11
+		int a=4;
+		int b=6;
+		int c=8;
+		
+		System.out.println (a+(a*b*c)^(1/7)-1/(1+1/(a+1/(b+1/(c+1/(a*b*c))))));
+	}
+
+}
