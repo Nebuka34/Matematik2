a147c374c50258a8183d0db1d5fc3c444dd8164e
BOSS <BOSS@DESKTOP-4M1S8HA>
2019-10-09 13:54:56 +0300

    mat

---
+++ matematik2/src/matematik2/Matematik9.java
@@ -0,0 +1,14 @@
+package matematik2;
+
+public class Matematik9 {
+	public static void main(String[] args) {
+		int a=4;
+		int b=6;
+		int c=8;
+		int d=10;
+		
+		
+		System.out.println ((a^5+b^3/(c+d))/((a+b^2+c^3+d^4)^(1/4)));
+	}
+}
+				
