ba63198f63f7c266efb9bd6d57c22246992a37fe
BOSS <BOSS@DESKTOP-4M1S8HA>
2019-10-09 09:09:32 +0300

    Matematik islemi 2

---
+++ matematik2/src/matematik2/Mat2.java
@@ -0,0 +1,14 @@
+package matematik2;
+
+public class Mat2 {
+
+	public static void main(String[] args) {
+		// matematik islemi 2 
+				int a=4;
+				int b=6;
+				int c=8;
+
+				System.out.println (a+b^2-c^3);
+	}
+
+}
